313eb4e962e9a21cbd213480065370c4f4bb2688
Michael Done <michael.done@sympatico.ca>
2019-11-09 19:17:29 -0500

    Implemented the updateDrawinHead function

--- main.c
+++ main.c
@@ -1,11 +1,25 @@
 #include "DrawingHead.h"
 
 // Low-level system reset. This should be accessing sensor and motor data directly rather than through DrawingHead
-void systemReset();
+void systemReset(); 
 
-// Creates a new structure to keep track of the drawing head's data
-struct DrawingHead newDrawingHead();
+// Run every loop to update the status of the drawing head with the real-world sensor and encoder data
+void updateDrawingHead(struct DrawingHead *head);
+
 
 task main(){
-    struct DrawingHead head = newDrawingHead();
+    struct DrawingHead head;
+    updateDrawingHead(&head);
+}
+
+void updateDrawingHead(struct DrawingHead *head) {
+    // Update motor encoders
+    head->x = nMotorEncoder[head->xAxisMotor];
+    head->y = nMotorEncoder[head->yAxisMotor];
+
+    // Detect whether or not it is drawing on the surface
+    head->drawing = nMotorEncoder[head->drawMotor] >= head->DRAW_MOTOR_LIMIT;
+
+    // Update the color
+    head->color = SensorValue[head->colorSensor];
 }
