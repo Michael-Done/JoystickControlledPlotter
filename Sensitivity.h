c91e8d277f7cc2cc24214175014aee80809e137e
LMedeir <55365178+LMedeir@users.noreply.github.com>
2019-11-15 09:00:30 -0500

    Added Sensitivity

---
+++ Sensitivity.h
@@ -0,0 +1,11 @@
+float setSensitivity()
+{
+	float sensitivity = nMotorEncoder[SENS_MOTOR] * SENSFACTOR;
+
+	if(sensitivity > 100)
+		return 100;
+	else if(sensitivity < 0)
+		return 0;
+
+	return sensitivity;
+}
