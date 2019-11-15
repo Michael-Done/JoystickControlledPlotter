a6c7ea2e1fa4242184b7c819893b5a424e31885b
Andrew-Tersigni <adtersigni@gmail.com>
2019-11-15 10:18:20 -0500

    completed systemReset

---
+++ systemReset.h
@@ -0,0 +1,22 @@
+void systemReset()
+{
+    motor[X_AXIS] = 0;
+    motor[Y_AXIS] = 0;
+
+    actuate(-1, 0);
+
+    nMotorEncoder[X_AXIS] = 0;
+    motor[X_AXIS] = 100;
+    while(SensorValue[COLOR] != (int)colorRed && nMotorEncoder[X_AXIS] < X_LIMIT)
+    {}
+    motor[X_AXIS] = 0;
+
+    nMotorEncoder[Y_AXIS] = 0;
+    motor[Y_AXIS] = -100;
+    while(SensorValue[COLOR] != (int)colorBlack && nMotorEncoder[Y_AXIS] < Y_LIMIT)
+    {}
+    motor[Y_AXIS] = 0;
+
+    resetGyro(X_GYRO);
+    resetGyro(Y_GYRO);
+}
