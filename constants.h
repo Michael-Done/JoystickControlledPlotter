df2635f0c891f097208d36c3a1b5f83b5130cdd3
LMedeir <55365178+LMedeir@users.noreply.github.com>
2019-11-15 09:18:24 -0500

    Fixing Sensitivity Direction

--- constants.h
+++ constants.h
@@ -3,7 +3,7 @@ const int X_LIMIT = 360*5; // The maximum x-axis encoder limit
 const int Y_LIMIT = 360*5; // The maximum y-axis encoder limit
 const int MOVE_TO_TOL = 2; // When the head is within this many ticks of a point, it is considered to be at the point
 const int SHAPE_SPEED = 50; // The speed at which the pre-programmed shapes will be drawn
-const float SENSFACTOR = 3*(100/360.0); //The % sensitivity the motor encoder changes
+const float SENSFACTOR = -3*(100/360.0); //The % sensitivity the motor encoder changes
 
 //const tSensors COLOR_SENSOR = S1; // The color sensor port
 //const tMotor X_AXIS = motorA; // The x-axis motor
