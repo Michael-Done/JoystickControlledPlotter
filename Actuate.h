db22258d6ff460367e46fa78dc72a45aa85ca58c
Abdullah Ghaith <abdullah.ghaith3@gmail.com>
2019-11-15 09:53:23 -0500

    Actuate V3.0 (While loop + control + Button State)

--- Actuate.h
+++ Actuate.h
@@ -1,17 +1,29 @@
-void actuate (int sign) //sign should be -1 or +1
+bool actuate (int sign, bool control) //sign should be -1 or +1
 //assuming user will not be able to move robot while it actuates
 {
+    int priorButtonState = SensorValue[TOUCH_SENSOR]; //buttonState = 0 as this function is called only when button has been released
+  
+    while ((nMotorEncoder[DRAW_MOTOR]<DRAW_MOTOR_LIMIT && sign > 0) || (nMotorEncoder[DRAW_MOTOR]>0 && sign < 0))//move based on actuation stage
+    {  
 
-    if(nMotorEncoder[DRAW_MOTOR]<DRAW_MOTOR_LIMIT && sign > 0 )//move until it reaches bottom boundary
-        motor[DRAW_MOTOR]=ACUTATE_SPEED*sign; 
+    motor[DRAW_MOTOR]=ACUTATE_SPEED*sign;
+
+    if (control)
+    {
+        joystickinput(gyro1,gyro2)
+
+        if (SensorValue[TOUCH_SENSOR]!=1 && priorButtonState==1)
+            sign=-sign;
+       
+    }
     
-    else if(nMotorEncoder[DRAW_MOTOR] > 0 && sign < 0 )
-        motor[DRAW_MOTOR]=ACUTATE_SPEED*sign; 
+    priorButtonState=SensorValue[TOUCH_SENSOR];
     
-    else if(nMotorEncoder[DRAW_MOTOR]>=DRAW_MOTOR_LIMIT || nMotorEncoder[DRAW_MOTOR]<=0)
+    }
+
+    if (nMotorEncoder[DRAW_MOTOR]>=DRAW_MOTOR_LIMIT || nMotorEncoder[DRAW_MOTOR]<=0)
         motor[DRAW_MOTOR]=0;
-
     
-    
+    return true;
     
 }
