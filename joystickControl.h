c22f34c53f50b087da0560ef260b8b90b1795f7c
Abdullah Ghaith <abdullah.ghaith3@gmail.com>
2019-11-16 13:07:34 -0500

    Joystick Control V2.0 (Final) Added X/Y Gyro limit constants

--- joystickControl.h
+++ joystickControl.h
@@ -3,18 +3,20 @@
        None.
 */
 #include "constants.h" //constant values file
-#include "Sensitivity.h" //sensitivity value generator 
+#include "Sensitivity.h" //sensitivity value generator
 #include "movement.h" //includes setSpeed
 
-void joystickControl()
+void joystickControl(int sensitivity_Val)
 {
     int xSpeed= 0; //will remain at 0 if the gyros are within the dead zone
     int ySpeed = 0;
 
-    if (abs(getGyroDegrees(X_GYRO))>5)
-        xSpeed=getGyroDegrees(X_GYRO)*setSensitivity();
-    if (abs(getGyroDegrees(Y_GYRO))>5)
-        ySpeed=getGyroDegrees(Y_GYRO)*setSensitivity();
-   
+    if (abs(getGyroDegrees(X_GYRO))>X_GYRO_LIMIT)
+        xSpeed=getGyroDegrees(X_GYRO)*sensitivity_Val;
+    if (abs(getGyroDegrees(Y_GYRO))>Y_GYRO_LIMIT)
+        ySpeed=getGyroDegrees(Y_GYRO)*sensitivity_Val;
+
+
     setSpeed(xSpeed,ySpeed);
+    wait1Msec(50);
 }
