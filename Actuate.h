fa547073a6e8f514111d4bc6b8574452dd702726
Abdullah Ghaith <abdullah.ghaith3@gmail.com>
2019-11-16 16:16:15 -0500

    Actuate V3.0 Final. Remade into bool

--- Actuate.h
+++ Actuate.h
@@ -1,32 +1,42 @@
 #include "constants.h"
 #include "joystickControl.h"
 
-void actuate (int sign, bool control) { //sign should be -1 or +1
-    int priorButtonState = SensorValue[TOUCH_SENSOR]; //buttonState = 0 as this function is called only when button has been released
-    while ((nMotorEncoder[DRAW_MOTOR]<DRAW_MOTOR_LIMIT && sign > 0) || (nMotorEncoder[DRAW_MOTOR]>10 && sign < 0)){//move based on actuation stage
-        motor[DRAW_MOTOR]=ACTUATE_SPEED*sign;
-        if (control){
-        //   joystickControl();
-            if (SensorValue[TOUCH_SENSOR]!=1 && priorButtonState==1)
-                sign=-sign;
-        }
+bool actuate (int sign, bool control) { //sign should be -1 or +1
+	int priorButtonState = 0; //buttonState = 0 as this function is called only when button has been released
+	while ((nMotorEncoder[DRAW_MOTOR]<DRAW_MOTOR_LIMIT && sign > 0) || (nMotorEncoder[DRAW_MOTOR]>10 && sign < 0))//move based on actuation stage
+	{
+		motor[DRAW_MOTOR]=ACTUATE_SPEED*sign;
 
-        priorButtonState=SensorValue[TOUCH_SENSOR];
-        wait1Msec(10);
-    }
-    motor[DRAW_MOTOR]=0;
+		if (control)
+		{
+			//   joystickControl();
+
+			if (SensorValue[TOUCH_SENSOR]!=1 && priorButtonState==1)
+				sign=-sign;
+
+			priorButtonState=SensorValue[TOUCH_SENSOR];
+			wait1Msec(10);
+		}
+	}
+
+	if ((nMotorEncoder[DRAW_MOTOR]>=DRAW_MOTOR_LIMIT)){
+		motor[DRAW_MOTOR]=5; //holding power
+		return true; //means it's acuated to bottom
+	}
+	else
+	{
+		motor[DRAW_MOTOR]=0;
+		return false; //means it's actuated to top
+	}
 }
 
 void checkActuate (bool & headStateUp, int & priorButtonState) {
 	if(SensorValue[TOUCH_SENSOR] != 1 && priorButtonState==1) {
-		if(headStateUp){
-			actuate(1, 1);
-			headStateUp=false;
-		}
-		else {
-			actuate(-1, 1);
-			headStateUp = true;
-		}
+		if(headStateUp)
+			headStateUp=actuate(1,true); //make actuate back into bool, headstate=actuate(1,101)
+		else
+			headStateUp = actuate(1,false);
+
 	}
 	priorButtonState=SensorValue[TOUCH_SENSOR];
 	wait1Msec(10);
